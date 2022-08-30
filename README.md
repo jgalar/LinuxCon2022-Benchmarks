e5053728b294e5479374d674c8d01a8ec07389c2
Jérémie Galarneau <jeremie.galarneau@efficios.com>
2022-08-30 14:43:29 -0400

    Add workload skeleton
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# Benchmark plan
+# Setup
 
+```sh
+# Install bcc, see https://github.com/iovisor/bcc/blob/master/INSTALL.md
+
+# Build workload
+$ mkdir build && cd build && cmake .. -DCMAKE_BUILD_TYPE=Release && make && cd ..
+
+# Setup python virtual env with poetry
+$ poetry install
+```
+
+# Running benchmarks
+
+```sh
+# ...
+```
---
+++ src/workload_tp.hpp
@@ -0,0 +1,23 @@
+#undef LTTNG_UST_TRACEPOINT_PROVIDER
+#define LTTNG_UST_TRACEPOINT_PROVIDER lc2022
+
+#if !defined(_TRACEPOINT_LC22_WORKLOAD_TP_H) || defined(LTTNG_UST_TRACEPOINT_HEADER_MULTI_READ)
+#define _TRACEPOINT_LC22_WORKLOAD_TP_H
+
+#include <lttng/tracepoint.h>
+#include <stddef.h>
+
+LTTNG_UST_TRACEPOINT_EVENT(lc2022, benchmark_event,
+	LTTNG_UST_TP_ARGS(unsigned int, anint),
+	LTTNG_UST_TP_FIELDS(
+		lttng_ust_field_integer(unsigned int, intfield, anint)
+	)
+)
+
+#endif /* _TRACEPOINT_LC22_WORKLOAD_TP_H */
+
+#undef LTTNG_UST_TRACEPOINT_INCLUDE
+#define LTTNG_UST_TRACEPOINT_INCLUDE "./workload_tp.hpp"
+
+/* This part must be outside ifdef protection */
+#include <lttng/tracepoint-event.h>
