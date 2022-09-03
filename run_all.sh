f1f0caaabc30a9eb7773623582700211f2d97e71
Jérémie Galarneau <jeremie.galarneau@efficios.com>
2022-09-03 17:55:01 -0400

    run_all: dump system config after benchmark run
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

--- run_all.sh
+++ run_all.sh
@@ -13,6 +13,38 @@ poetry install
         poetry run bench --workload build/workload --duration $DURATION --iteration-count $ITER_COUNT lttng-ust-ringbuffer --num-subbuf 4 --subbuf-size 4K
         poetry run bench --workload build/workload --duration $DURATION --iteration-count $ITER_COUNT lttng-kernel-ringbuffer --num-subbuf 4 --subbuf-size 4K
 
-        poetry run bench --workload build/workload --duration $DURATION --iteration-count $ITER_COUNT lttng-ust-ringbuffer --num-subbuf 4 --subbuf-size 4M
-        poetry run bench --workload build/workload --duration $DURATION --iteration-count $ITER_COUNT lttng-kernel-ringbuffer --num-subbuf 4 --subbuf-size 4M
+        poetry run bench --workload build/workload --duration $DURATION --iteration-count $ITER_COUNT lttng-ust-ringbuffer --num-subbuf 4 --subbuf-size 8M
+        poetry run bench --workload build/workload --duration $DURATION --iteration-count $ITER_COUNT lttng-kernel-ringbuffer --num-subbuf 4 --subbuf-size 8M
+
+        echo "Machine summary"
+        echo "---------------"
+        echo ""
+        echo "cpu info"
+        echo "--------"
+        cat /proc/cpuinfo
+        echo ""
+        echo "mem info"
+        echo "--------"
+        cat /proc/meminfo
+        echo ""
+        echo "numa info"
+        echo "---------"
+        numactl --hardware
+        echo ""
+        echo "kernel:"
+        echo "-------"
+        uname -a
+        echo ""
+        echo "distro info:"
+        echo "------------"
+        lsb_release -a
+        echo ""
+        echo "distro info:"
+        echo "------------"
+        lsb_release -a
+        echo ""
+        echo "kernel cfg:"
+        echo "-----------"
+        zcat /proc/config.gz
+        cat "/boot/config-$(uname -r)"
 } > results
